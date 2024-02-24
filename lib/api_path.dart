7a5a52720b4a1324e8e7e6053c432d0fce704e15
AungBoMyint <aungbomyint292@gmail.com>
2024-02-24 08:13:16 +0630

    change domain

--- lib/api_path.dart
+++ lib/api_path.dart
@@ -1,29 +1,48 @@
-const String categoryPath = "https://kznidea.online/learning/categories/";
-const String sliderPath = "https://kznidea.online/learning/sliders/";
-const String coursePath = "https://kznidea.online/learning/courses/";
-const String discountPath = "https://kznidea.online/learning/discounts/";
+const String categoryPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/categories/";
+const String sliderPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/sliders/";
+const String coursePath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/courses/";
+const String discountPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/discounts/";
 const String discountItemPath =
-    "https://kznidea.online/learning/discount_items/";
-const String youtubeLinkPath = "https://kznidea.online/learning/youtube_links/";
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/discount_items/";
+const String youtubeLinkPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/youtube_links/";
 const String facebookLinkPath =
-    "https://kznidea.online/learning/facebook_links/";
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/facebook_links/";
 const String messengerLinkPath =
-    "https://kznidea.online/learning/messenger_links/";
-const String courseLinkPath = "https://kznidea.online/learning/courselinks/";
-const String blogLinkPath = "https://kznidea.online/learning/blog_links/";
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/messenger_links/";
+const String courseLinkPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/courselinks/";
+const String blogLinkPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/blog_links/";
 const String adminStudentPath =
-    "https://kznidea.online/learning/admin_students/";
-const String sectionPath = "https://kznidea.online/learning/sections/";
-const String subsectionPath = "https://kznidea.online/learning/subsections/";
-const String videoPath = "https://kznidea.online/learning/videos/";
-const String blogPath = "https://kznidea.online/learning/blogs/";
-const String filePath = "https://kznidea.online/learning/pdfs/";
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/admin_students/";
+const String sectionPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/sections/";
+const String subsectionPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/subsections/";
+const String videoPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/videos/";
+const String blogPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/blogs/";
+const String filePath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/pdfs/";
 const String enrollmentPath =
-    "https://kznidea.online/learning/admin_enrollments/";
-const String ratingPath = "https://kznidea.online/learning/admin_ratings/";
-const String reviewPath = "https://kznidea.online/learning/admin_reviews/";
-const String profilePath = "https://kznidea.online/learning/students/me/";
-const String signUpPath = "https://kznidea.online/auth/users/";
-const String signInPath = "https://kznidea.online/auth/jwt/create/";
-const String studentsPath = "https://kznidea.online/learning/students/";
-const String updateProfilePath = "https://kznidea.online/learning/students/me/";
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/admin_enrollments/";
+const String ratingPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/admin_ratings/";
+const String reviewPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/admin_reviews/";
+const String profilePath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/students/me/";
+const String signUpPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/auth/users/";
+const String signInPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/auth/jwt/create/";
+const String studentsPath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/students/";
+const String updateProfilePath =
+    "https://idea-api-gzdcu.ondigitalocean.app/learning/students/me/";
